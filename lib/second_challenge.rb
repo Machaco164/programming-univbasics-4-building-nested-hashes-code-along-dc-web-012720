670355f9666d0c83cdd2fb5ce2497521606e61cb
Raul A Rozo <raul.a.rozo.o@gmail.com>
2020-01-15 01:25:10 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,7 +2,7 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       :patriarch => {},
-      :matriarcg => {},
+      :matriarch => {},
       :hero => {},
       :hero_friends => []
       }, 
